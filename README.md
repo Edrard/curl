6c61c5ee22c2ea8cf149fe0daad0a7a210396d94
Edrard A <demonand@gmail.com>
2018-08-08 00:19:04 +0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Curl
 Small Curl library with Multicurl
+
+
+Simple Example
+```
+$urls[] = 'https://microsoft.com';
+$urls[] = 'https://google.com';
+
+$curl = new edrard\Curl\Curl();
+$curl->setSleep(function($retry){
+   return ceil($retry/50); 
+});
+//Adding Urls
+foreach($urls as $key => $link){
+    $curl->addSession( $link, $key );
+}
+// In $tmp we have result
+$tmp = $curl->exec();
+```
--- src/Curl/GetUrl.php
+++ src/Curl/GetUrl.php
@@ -12,17 +12,17 @@ class GetUrl
         $res = FALSE;
         while( $retry >= 0 && $res === FALSE) 
         {
+            if(!$url){
+                $res = '';
+                break;
+            } 
             $sleep = $function !== NULL ? $function($retry) : 1;
             sleep($sleep);
             $ctx = stream_context_create(array('http'=>
                 array(
                     'timeout' => 15, // 1 200 Seconds = 20 Minutes
                 )
-            ));
-            if(!$url){
-                $res = '';
-                break;
-            }   
+            ));  
             $res = file_get_contents($url, false, $ctx);  
             $retry--;
         }
